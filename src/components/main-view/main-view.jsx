bbf2d47f845a2ddb82dbce03d0457ec091586fe6
meerkatstudio <patrick.lemmer@yahoo.com>
2022-07-07 19:22:20 +0200

    replaced hardcoded movie array with API data

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,11 @@
 // React imports
 import React from 'react';
 
+// Other imports
+import axios from 'axios';
+
 // Component imports
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -9,44 +13,56 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-        {
-          _id: 1,
-          Title: 'Inception',
-          Description: 'desc1...',
-          ImagePath: '...',
-        },
-        {
-          _id: 2,
-          Title: 'The Shawshank Redemption',
-          Description: 'desc2...',
-          ImagePath: '...',
-        },
-        {
-          _id: 3,
-          Title: 'Gladiator',
-          Description: 'desc3...',
-          ImagePath: '...',
-        },
-      ],
+      movies: [],
       selectedMovie: null,
+      user: null,
     };
   }
 
-  setSelectedMovie(newSelectedMovie) {
+  componentDidMount() {
+    axios
+      .get('https://patricklemmer-myflix.herokuapp.com/movies')
+      .then((response) => {
+        this.setState({
+          movies: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie
+  setSelectedMovie(movie) {
     this.setState({
-      selectedMovie: newSelectedMovie,
+      selectedMovie: movie,
+    });
+  }
+
+  // When a user successfully logs in, this function updates the `user` property in state to that *particular user
+  onLoggedIn(user) {
+    this.setState({
+      user,
     });
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
 
-    if (movies.length === 0)
-      return <div className="main-view">The list is empty!</div>;
+    {
+      /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView */
+    }
+    if (!user)
+      return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
+
+    {
+      /* Before the movies have been loaded */
+    }
+    if (movies.length === 0) return <div className="main-view" />;
 
     return (
       <div className="main-view">
+        {/* If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned */}
         {selectedMovie ? (
           <MovieView
             movie={selectedMovie}
