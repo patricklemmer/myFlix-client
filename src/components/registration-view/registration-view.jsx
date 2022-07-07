aef8b06bc99f4a3f29df9dfe9c434666528d6874
meerkatstudio <patrick.lemmer@yahoo.com>
2022-07-07 19:19:38 +0200

    created login and registration views

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        Birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit" onClick={handleSubmit}>
+        Register
+      </button>
+    </form>
+  );
+}
